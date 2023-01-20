469dd39f4e01bfabdede27d082928dfb1628ab50
lizihong <lizihong@km-it.cn>
2023-01-20 15:06:27 +0800

    增加代码

---
+++ c/data_structure/list_node.h
@@ -0,0 +1,7 @@
+// 链表节点
+struct ListNode {
+    // 值
+    int val;
+    // 下一个节点
+    struct ListNode *next;
+};
---
+++ c/data_structure/tree.h
@@ -0,0 +1,9 @@
+// 二叉树
+struct TreeNode {
+    // 值
+    int val;
+    // 左节点
+    struct TreeNode *left;
+    // 右节点
+    struct TreeNode *right;
+};
